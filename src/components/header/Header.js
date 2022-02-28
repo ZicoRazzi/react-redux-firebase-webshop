6e41cd400993ba0a99489306ea4e99a161ef595e
ZicoRazzi <zicorazzi@gmail.com>
2022-02-28 15:52:15 +0100

    header update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 import Navigation from '../navigation/Navigation';
 import './styles.scss';
 
 const Header = (props) => {
+  const [showHeader, setShowHeader] = useState(true);
+
+  const handleShowHeader = () => {
+    if (window.scrollY > 80) {
+      setShowHeader(false);
+    } else {
+      setShowHeader(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleShowHeader);
+    return () => {
+      window.removeEventListener('scroll', handleShowHeader);
+    };
+  }, []);
+
   return (
     <div>
-      <header className="header">
+      <header className={`header ${showHeader && 'header-scroll'}`}>
         <div className="logo">
           <Link className="logo-link" to="/">
             Le Bonnet
